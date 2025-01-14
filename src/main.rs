a8405547bf5ac3ded3478bc6bbe8d9dcd50f530b
Odin Thif <mjd.thif@icloud.com>
2025-01-14 12:43:05 +0100

    rust practic is done for today.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let  arr = [1,2,3,4,6,7,8,15,16];
+    println!("Array: {:?}", arr);
+    for i in 0..arr.len() {
+        if arr[i+1] - arr[i] != 1 {
+            println!("Element at index {} is {}", i+1, arr[i+1]);
+        }
+    
+    }
+
 }
