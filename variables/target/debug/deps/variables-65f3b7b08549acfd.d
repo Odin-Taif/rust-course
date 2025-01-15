8e5e556d1d42fcad732473789058d4df90744ee4
Odin Thif <mjd.thif@icloud.com>
2025-01-15 12:41:26 +0100

    starting rust by example

---
+++ variables/target/debug/deps/variables-65f3b7b08549acfd.d
@@ -0,0 +1,5 @@
+/Users/mjdthif/Desktop/rust-practice/variables/target/debug/deps/variables-65f3b7b08549acfd: src/main.rs
+
+/Users/mjdthif/Desktop/rust-practice/variables/target/debug/deps/variables-65f3b7b08549acfd.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/mjdthif/Desktop/rust-practice/variables/target/debug/variables: /Users/mjdthif/Desktop/rust-practice/variables/src/main.rs
